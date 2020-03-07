39fd1a3d9e5b5f73d2a79f072ed100710d0a9bf2
yinxulai <yinxulai@hotmail.com>
2020-03-07 15:05:23 +0800

    优化代码

--- models/label.go
+++ models/label.go
@@ -10,9 +10,9 @@ import (
 type Label struct {
 	ID          sql.NullInt64
 	Name        sql.NullString
-	Category       sql.NullString
 	State       sql.NullString
 	Value       sql.NullString
+	Category    sql.NullString
 	DeletedTime sql.NullTime
 	CreatedTime sql.NullTime
 	UpdatedTime sql.NullTime
@@ -22,9 +22,9 @@ type Label struct {
 func (srv *Label) LoadProtoStruct(label *standard.Label) {
 	srv.ID.Scan(label.ID)
 	srv.Name.Scan(label.Name)
-	srv.Category.Scan(label.Category)
 	srv.State.Scan(label.State)
 	srv.Value.Scan(label.Value)
+	srv.Category.Scan(label.Category)
 	srv.DeletedTime.Scan(label.DeletedTime)
 	srv.CreatedTime.Scan(label.CreatedTime)
 	srv.UpdatedTime.Scan(label.UpdatedTime)
@@ -34,9 +34,9 @@ func (srv *Label) LoadProtoStruct(label *standard.Label) {
 func (srv *Label) LoadStringMap(data map[string]string) {
 	srv.ID.Scan(data["ID"])
 	srv.Name.Scan(data["Name"])
-	srv.Category.Scan(data["Category"])
 	srv.State.Scan(data["State"])
 	srv.Value.Scan(data["Value"])
+	srv.Category.Scan(data["Category"])
 	srv.DeletedTime.Scan(data["DeletedTime"])
 	srv.CreatedTime.Scan(data["CreatedTime"])
 	srv.UpdatedTime.Scan(data["UpdatedTime"])
@@ -47,9 +47,9 @@ func (srv *Label) OutProtoStruct() *standard.Label {
 	lable := new(standard.Label)
 	lable.ID = srv.ID.Int64
 	lable.Name = srv.Name.String
-	lable.Category = srv.Category.String
 	lable.State = srv.State.String
 	lable.Value = srv.Value.String
+	lable.Category = srv.Category.String
 	lable.DeletedTime = srv.DeletedTime.Time.String()
 	lable.CreatedTime = srv.CreatedTime.Time.String()
 	lable.UpdatedTime = srv.UpdatedTime.Time.String()
