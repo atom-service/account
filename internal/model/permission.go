ee06b366ecc469090c918e556a0a39a8ea62d60d
yinxulai <yinxulai@hotmail.com>
2024-03-12 18:20:31 +0800

    WIP

--- internal/model/permission.go
+++ internal/model/permission.go
@@ -1,1 +1,20 @@
 package model
+
+// message RoleResource {
+//   string Resource = 1;
+//   bool Insert = 2;
+//   bool Delete = 3;
+//   bool Update = 4;
+//   bool Query  = 5;
+// }
+
+// message Role {
+//   string ID = 1;
+//   string Name = 2;
+//   string Description = 3;
+//   repeated RoleResource Resources = 4;
+
+//   string CreatedTime = 5;
+//   string UpdatedTime = 6;
+//   optional string DeletedTime = 7;
+// }
